472f594f00614d51c57c00f9dd50e66ecaf62c18
felitech26 <felitech26@gmail.com>
2024-03-24 13:13:23 +0000

    Update Category.jsx

--- components/Category.jsx
+++ components/Category.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const Category = () => {
   return (
-    <section className="pb-[5rem] px-[5rem]"> 
+    <section className="pb-[5rem]"> 
      <h5 className="mb-5 text-[30px] md:text-[40px] text-center
       uppercase">Shop By Categories</h5> 
 
