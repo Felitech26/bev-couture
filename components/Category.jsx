e52db3ce85f2fb50bd2a1f8d7a1cf3f3139588de
felitech26 <felitech26@gmail.com>
2024-03-24 13:01:59 +0000

    Update Category.jsx

--- components/Category.jsx
+++ components/Category.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const Category = () => {
   return (
-    <section className="pb-[5rem]"> 
+    <section className=" pb-[5rem]"> 
      <h5 className="mb-5 text-[30px] md:text-[40px] text-center
       uppercase">Shop By Categories</h5> 
 
@@ -15,19 +15,19 @@ const Category = () => {
       </h5>
       </div>
 
-      <div className="flex flex-col lg:flex-row justify-center items-center px-3 gap-7">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-7">
         <div className="group overflow-hidden flex justify-center items-center">
-        <div className="w-[500px] h-[600px] group-hover:scale-110 transition-all duration-500">
+        <div className="w-[420px] h-[550px] group-hover:scale-110 transition-all duration-500">
         <div className="absolute">
       <Image src={'/continental.png'}
        alt=""
-       width={500}
-       height={600} 
-       className="object-cover w-[500px] h-[600px]"
+       width={400}
+       height={300} 
+       className="object-cover w-[420px] h-[550px]"
       />
       </div>
-      <div className="relative w-[500px] h-[600px] bg-black/25 
-      flex justify-center items-end group-hover:-translate-y-7 
+      <div className="relative w-[420px] h-[550px] bg-black/25 
+      flex justify-center items-end group-hover:-translate-y-6 
      transition-all duration-500">
         <div className="flex flex-col justify-center items-center 
         
@@ -43,7 +43,7 @@ const Category = () => {
         </div>
         </div>
         <div className="group overflow-hidden flex justify-center items-center">
-        <div className="w-[500px] h-[600px] group-hover:scale-110 transition-all duration-500">
+        <div className="w-[400px] h-[600px] group-hover:scale-110 transition-all duration-500">
         <div className="absolute">
       <Image src={'/casual.png'}
        alt=""
