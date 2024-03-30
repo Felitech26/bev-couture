ce0f72fbc4fe098e3b4332e132d5ffea8f9aa287
felitech26 <felitech26@gmail.com>
2024-03-30 15:17:02 +0000

    G

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -9,36 +9,31 @@ import Link from "next/link";
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState(''); 
 
   return (
     <div className="h-screen py-[10rem]">
     <div className="flex justify-center">
     <div className="flex flex-col">
       <h5 className="text-4xl font-semibold mb-5">Sign In</h5>
-      <div className="flex flex-col gap-4">
+      <form className="grid place-content-center gap-4">
       <input 
-      id="email"
-      type="email"
-      label="Email"
-      placeholder="Email" className="border border-solid
+      type="text" placeholder="Email" className="border border-solid
   py-3 px-4 w-[345px] sm:w-[500px] lg:w-[420px] outline-none border-black/50
    text-lg placeholder:text-[#6b6a6a] placeholder:text-md"/>
-      <input 
-      id="password"
-      type="password"
-      label="Password"
-      placeholder="Email" className="border border-solid
+      <input
+      type="password" placeholder="Password" className="border border-solid
   py-3 px-4 w-[345px] sm:w-[500px] lg:w-[420px] outline-none border-black/50
    text-lg placeholder:text-[#6b6a6a] placeholder:text-md"/>
 
-      </div>
-
-      <button className="bg-black mt-[3rem]
+<button className="bg-black mt-[2rem]
   px-3 py-3 border border-xl text-white border-black text-[20px]
   hover:bg-white hover:text-black">Sign In</button>
 
-    <div className="mt-5 flex gap-2">   
+  <div className="border border-solid w-fit text-sm 
+   py-2 px-3 rounded-sm">Error Messsage </div>
+
+<div className="mt-3 flex justify-center gap-2">   
   <h5 className="text-lg font-medium">
   New customer?
   </h5>
@@ -47,6 +42,12 @@ const SignIn = () => {
      <h5 className="text-lg font-medium">Create New Account</h5>
   </Link>
     </div>
+
+      </form>
+
+
+
+  
     </div>
     </div>
    
--- components/SignUp.jsx
+++ components/SignUp.jsx
@@ -1,11 +1,85 @@
+'use client'
+
+import Link from "next/link"
+import { useState } from "react"
+
+
 const SignUp = () => {
+ const [name, setName] = useState('')
+ const [email, setEmail] = useState('')
+ const [password, setPassword] = useState('')
+ const [error, setError] = useState('')
+
+const handleSubmit = (e) => {
+e.preventDefault();
+
+if (!name || !email || !password) {
+  setError("All fields are required.");
+  return;
+}
+
+
+
+};
+
+
+
   return (
-    <div className="h-screen py-56">
-      
-      <h5 className="text-[65px]">SignUp</h5>
-      
-      </div>
+    <div className="h-screen py-[10rem]">
+    <div className="flex justify-center">
+    <div className="flex flex-col">
+      <h5 className="text-4xl font-semibold mb-5">Sign Up</h5>
+      <form 
+      onSubmit={handleSubmit}
+      className="grid place-content-center gap-4">
+
+      <input onChange={(e) => setName(e.target.value)}
+     
+      type="text" placeholder="Full Name" className="border border-solid
+  py-3 px-4 w-[345px] sm:w-[500px] lg:w-[420px] outline-none border-black/50
+   text-lg placeholder:text-[#6b6a6a] placeholder:text-md"/>
+
+      <input onChange={(e) => setEmail(e.target.value)}
+      type="text" placeholder="Email" className="border border-solid
+  py-3 px-4 w-[345px] sm:w-[500px] lg:w-[420px] outline-none border-black/50
+   text-lg placeholder:text-[#6b6a6a] placeholder:text-md"/>
+      <input onChange={(e) => setPassword(e.target.value)}
+      type="password" placeholder="Password" className="border border-solid
+  py-3 px-4 w-[345px] sm:w-[500px] lg:w-[420px] outline-none border-black/50
+   text-lg placeholder:text-[#6b6a6a] placeholder:text-md"/>
+
+<button className="bg-black mt-[2rem]
+  px-3 py-3 border border-xl text-white border-black text-[20px]
+  hover:bg-white hover:text-black">Sign Up</button>
+
+  { error && (
+    <div className="border border-solid w-fit text-md
+    py-2 px-3 rounded-sm">{error}</div>
   )
+    
+  }
+
+<div className="mt-3 flex justify-center gap-2">   
+  <h5 className="text-lg font-medium">
+ Already have account?
+  </h5>
+
+  <Link href='/signin'>
+     <h5 className="text-lg font-medium">Login</h5>
+  </Link>
+    </div>
+
+      </form>
+
+
+    
+    </div>
+    </div>
+   
+  </div>
+
+  )
+
 }
 
 export default SignUp
