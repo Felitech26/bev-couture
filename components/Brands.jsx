9013a14254a9877319f9d03b7e90079acba198b0
felitech26 <felitech26@gmail.com>
2024-04-22 12:22:30 +0000

    Update Brands.jsx

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -4,67 +4,40 @@ import React from 'react'
 
 const Brands = () => {
   return (
-    <section className='container mx-auto py-5 overflow-hidden'>
-
-<h5 className='text-center md:hidden text-[20px] 
-font-cormomorantGaramond
-font-bold'>Feautures</h5>
-   <div className='px-[3rem] grid md:hidden grid-cols-2 '>
-   <Image src={'/Forbes.png'} 
-      width={150}
-      height={150}
-      alt='elle'
-      />   
-   <Image src={'/Elle.png'} 
-      width={150}
-      height={150}
-      alt='elle'
-      />   
-
-   <Image src={'/Bbc.png'} 
-      width={150}
-      height={150}
-      alt='elle'
-      className='mb-2'
-      />  
-
-<Image src={'/Vogue.png'} 
-      width={150}
-      height={150}
-      alt='elle'/>
-    
-    </div> 
-
-
-
+    <section className='container py-5 overflow-hidden'>
 <h5 className='text-[20px] 
-font-cormomorantGaramond sm:hidden md:flex justify-center items-center
+font-cormomorantGaramond text-center
 font-bold'>Feautures</h5>
    <div className='flex flex-row justify-center items-center gap-10 
-   lg:gap-[5rem] sm:hidden md:flex'>
+   lg:gap-[5rem]'>
    <Image src={'/Forbes.png'} 
       width={150}
       height={150}
       alt='elle'
+      className='w-[70px]'
       />   
    <Image src={'/Elle.png'} 
       width={150}
       height={150}
       alt='elle'
+      className='w-[70px]'
       />   
 
    <Image src={'/Bbc.png'} 
       width={150}
       height={150}
       alt='elle'
-      className='mb-2'
+      className='w-[70px]'
+      
       />  
 
 <Image src={'/Vogue.png'} 
       width={150}
       height={150}
-      alt='elle'/>
-    
+      alt='elle'
+      className='w-[70px]'
+      />
+      
     </div> 
       
     </section>
