7d1823f383e5dd265c4f1d818b075a7a694d559c
felitech26 <felitech26@gmail.com>
2024-04-22 17:08:41 +0000

    k

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -4,9 +4,47 @@ import React from 'react'
 
 const Brands = () => {
   return (
-    <section className='container py-5 overflow-hidden'>
-   <div className='flex flex-row justify-center items-center gap-5 
-   lg:gap-[5rem]'>
+    <section className='py-5 overflow-hidden'>
+    <div className='flex space-x-[21rem] lg:space-x-[25rem] overflow-hidden'>
+    <div className='flex justify-center items-center space-x-8 
+    lg:space-x-16 animate-loop-scroll'>
+   <Image src={'/Forbes.png'} 
+      width={150}
+      height={150}
+      alt='forbes'
+      className='w-[70px] md:w-[100px] lg:w-[150px] max-w-none'
+      />   
+   <Image src={'/Elle.png'} 
+      width={150}
+      height={150}
+      alt='elle'
+      className='w-[75px] md:w-[100px] lg:w-[150px]'
+      />   
+
+   <Image src={'/Bbc.png'} 
+      width={150}
+      height={150}
+      alt='bbc'
+      className='w-[75px] md:w-[100px] lg:w-[150px] mb-1'
+      
+      />  
+
+   <Image src={'/Vogue.png'} 
+      width={150}
+      height={150}
+      alt='vogue'
+      className='w-[75px] md:w-[100px] lg:w-[150px]'
+      />
+   <Image src={'/Glitz.png'} 
+      width={150}
+      height={150}
+      alt='vogue'
+      className='w-[75px] md:w-[100px] lg:w-[150px]'
+      />
+      
+    </div> 
+<div className='flex space-x-8 lg:space-x-16 justify-center items-center 
+animate-loop-scroll aria-hidden:"true"'>
    <Image src={'/Forbes.png'} 
       width={150}
       height={150}
@@ -28,7 +66,13 @@ const Brands = () => {
       
       />  
 
-<Image src={'/Vogue.png'} 
+  <Image src={'/Vogue.png'} 
+      width={150}
+      height={150}
+      alt='vogue'
+      className='w-[75px] md:w-[100px] lg:w-[150px]'
+      />
+  <Image src={'/Glitz.png'} 
       width={150}
       height={150}
       alt='vogue'
@@ -36,6 +80,8 @@ const Brands = () => {
       />
       
     </div> 
+    </div>
+  
       
     </section>
    
