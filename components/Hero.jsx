a73842b04e84d7b88a657d2856fa65d0fc1831c5
felitech26 <felitech26@gmail.com>
2024-07-30 17:10:07 +0000

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,12 +5,12 @@ const Hero = () => {
   return (
     <section className='overflow-hidden pb-5'>
      <div>
-      <div className='h-[900px] lg:h-[1700px] xxl:w-full'>
+      <div className='h-[900px] lg:h-[1700px] xxl:w-[2200px] xxl:mt-7'>
         <Image src={'/hero4.png'}
         width={2000}
         height={1280}
         alt=''
-        className='h-[900px] object-cover lg:h-[1700px] xxl:w-full'
+        className='h-[900px] object-cover lg:h-[1700px] xxl:w-[2200px]'
          />
 
       </div>
